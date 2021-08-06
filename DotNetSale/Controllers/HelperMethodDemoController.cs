e2e451f7d473432dde6f6806aea4e9ec600b1b7c
psd-inje <parksd74@hanmail.net>
2021-08-06 14:08:17 +0900

    27_00

---
+++ DotNetSale/Controllers/HelperMethodDemoController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetSale.Controllers
+{
+    public class HelperMethodDemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult FormDemo()
+        {
+            return View();
+        }
+
+        public IActionResult InputDemo()
+        {
+            return View();
+        }
+
+        public IActionResult StronglyTypedDemo()
+        {
+            var stc = new StronglyTypedClass()
+            {
+                Id = 1,Name = "홍길동", Age = 21
+
+            };
+            return View(stc);
+        }
+
+    }
+}
+
+
+public class StronglyTypedClass
+{
+    public int Id { get; set; }
+    public string  Name { get; set; }
+    public int Age { get; set; }
+}
