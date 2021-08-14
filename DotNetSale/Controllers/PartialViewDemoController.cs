3df577b1ee1794975a82aacb6ccdf77566808933
psd-inje <parksd74@hanmail.net>
2021-08-14 19:29:09 +0900

    29_10

---
+++ DotNetSale/Controllers/PartialViewDemoController.cs
@@ -0,0 +1,33 @@
+﻿using DotNetSale.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetSale.Controllers
+{
+    public class PartialViewDemoController : Controller
+    {
+        public IActionResult Index(int Page = 0)
+        {
+            var pageModel = new PagerBase
+            {
+                Url = "PartialViewDemo",
+                RecordCount = 200,
+                PageSize = 10,
+                PageNumber = Page,
+
+                SearchMode = true,
+                SearchField = "Title",
+                SearchQuery  = "ASP.NET"
+            };
+
+            ViewBag.PageModel = pageModel;
+            
+
+
+            return View();
+        }
+    }
+}
