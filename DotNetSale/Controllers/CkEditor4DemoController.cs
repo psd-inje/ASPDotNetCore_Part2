5686aa9eb81bf33e5400bdc3006ff3a74ee09d70
psd-inje <parksd74@hanmail.net>
2021-08-15 18:53:41 +0900

    31_04

---
+++ DotNetSale/Controllers/CkEditor4DemoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetSale.Controllers
+{
+    public class CkEditor4DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Index(string editor)
+        {
+
+            ViewBag.EditorText = editor;
+            return View();
+        }
+    }
+}
